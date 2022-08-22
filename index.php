7ab9f1415718e8e73dd877410236a9d78d608b14
AlisePetersone <alise.jevstafjeva@gmail.com>
2022-08-22 15:03:15 +0300

    footer

---
+++ index.php
@@ -0,0 +1,80 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book.Smart</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<!-- footer starts -->
+
+<section class="footer">
+
+<div class="box-container">
+
+<div class="box">
+   <h3>quick links</h3>
+   <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+   <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+   <a href="books.php"> <i class="fas fa-angle-right"></i> books</a>
+   <a href="new-relises.php"> <i class="fas fa-angle-right"></i> new relises</a>
+</div>
+
+<div class="box">
+   <h3>extra links</h3>
+   <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+   <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+   <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+   <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+</div>
+
+<div class="box">
+   <h3>contact info</h3>
+   <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+   <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+   <a href="#"> <i class="fas fa-envelope"></i> books@booksmart.com </a>
+   <a href="#"> <i class="fas fa-map"></i> Riga, Latvia </a>
+</div>
+
+<div class="box">
+   <h3>follow us</h3>
+   <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+   <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+   <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+   <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+</div>
+
+</div>
+
+<div class="credit"> created by <span>Lasma Elvira Alise</span> | all rights reserved! </div>
+
+
+</section>
+
+<!-- footer ends -->
+
+<script src="js/script.js"></script>
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
